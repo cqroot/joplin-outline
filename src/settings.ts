9c1d0a3b2b383c1c16032db2a5784d8fc3f592f8
cqroot <cqroot@outlook.com>
2021-08-09 23:35:23 +0800

    build: use registerSettings instead of registerSetting

--- src/settings.ts
+++ src/settings.ts
@@ -7,143 +7,145 @@ export async function registerSettings() {
     iconName: 'fas fa-bars',
   });
 
-  await joplin.settings.registerSetting('toggleShortcut', {
-    type: SettingItemType.String,
-    value: 'F9',
-    description: 'Shortcut to switch panel display',
-    section: 'outline.settings',
-    public: true,
-    label: 'Toggle Shortcut',
-  });
-  await joplin.settings.registerSetting('autoHide', {
-    type: SettingItemType.Bool,
-    value: false,
-    description: 'Automatically hide the panel when there is no content',
-    section: 'outline.settings',
-    public: true,
-    label: 'Auto Hide',
-  });
-  await joplin.settings.registerSetting('headerDepth', {
-    type: SettingItemType.Int,
-    value: 6,
-    description: 'Header depth',
-    section: 'outline.settings',
-    public: true,
-    label: 'Header Depth',
-  });
-  await joplin.settings.registerSetting('fontFamily', {
-    type: SettingItemType.String,
-    value: 'var(--joplin-font-family)',
-    description: 'var(--joplin-font-family)',
-    section: 'outline.settings',
-    public: true,
-    label: 'Font Family',
-  });
-  await joplin.settings.registerSetting('fontSize', {
-    type: SettingItemType.Int,
-    value: '10',
-    description: 'Default: 10pt',
-    section: 'outline.settings',
-    public: true,
-    label: 'Font Size (pt)',
-  });
-  await joplin.settings.registerSetting('fontWeight', {
-    type: SettingItemType.String,
-    value: 'normal',
-    description: 'bold/normal/lighter',
-    section: 'outline.settings',
-    public: true,
-    label: 'Font Weight',
-  });
-  await joplin.settings.registerSetting('fontColor', {
-    type: SettingItemType.String,
-    value: 'var(--joplin-color)',
-    description: 'var(--joplin-color)',
-    section: 'outline.settings',
-    public: true,
-    label: 'Font Color',
-  });
-  await joplin.settings.registerSetting('bgColor', {
-    type: SettingItemType.String,
-    value: 'var(--joplin-background-color)',
-    description: 'var(--joplin-background-color)',
-    section: 'outline.settings',
-    public: true,
-    label: 'Background Color',
-  });
-  await joplin.settings.registerSetting('disableLinewrap', {
-    type: SettingItemType.Bool,
-    value: false,
-    description: 'Disable the linewrap',
-    section: 'outline.settings',
-    public: true,
-    label: 'Disable Linewrap',
-  });
-  await joplin.settings.registerSetting('showNumber', {
-    type: SettingItemType.Bool,
-    value: false,
-    description: 'show numbered headers',
-    section: 'outline.settings',
-    public: true,
-    label: 'Show Number',
-    advanced: true,
-  });
-  await joplin.settings.registerSetting('numberStyle', {
-    type: SettingItemType.String,
-    value: 'font-weight: normal; font-style: normal',
-    description: 'font-weight: normal; font-style: normal',
-    section: 'outline.settings',
-    public: true,
-    label: 'Number <i> Style',
-    advanced: true,
-  });
-  await joplin.settings.registerSetting('h1Prefix', {
-    type: SettingItemType.String,
-    value: '',
-    section: 'outline.settings',
-    public: true,
-    label: 'H1 Prefix',
-    advanced: true,
-  });
-  await joplin.settings.registerSetting('h2Prefix', {
-    type: SettingItemType.String,
-    value: '',
-    section: 'outline.settings',
-    public: true,
-    label: 'H2 Prefix',
-    advanced: true,
-  });
-  await joplin.settings.registerSetting('h3Prefix', {
-    type: SettingItemType.String,
-    value: '',
-    section: 'outline.settings',
-    public: true,
-    label: 'H3 Prefix',
-    advanced: true,
-  });
-  await joplin.settings.registerSetting('h4Prefix', {
-    type: SettingItemType.String,
-    value: '',
-    section: 'outline.settings',
-    public: true,
-    label: 'H4 Prefix',
-    advanced: true,
-  });
-  await joplin.settings.registerSetting('h5Prefix', {
-    type: SettingItemType.String,
-    value: '',
-    section: 'outline.settings',
-    public: true,
-    label: 'H5 Prefix',
-    advanced: true,
-  });
-  await joplin.settings.registerSetting('h6Prefix', {
-    type: SettingItemType.String,
-    value: '',
-    section: 'outline.settings',
-    public: true,
-    label: 'H6 Prefix',
-    advanced: true,
+  await joplin.settings.registerSettings({
+    toggleShortcut: {
+      type: SettingItemType.String,
+      value: 'F9',
+      description: 'Shortcut to switch panel display',
+      section: 'outline.settings',
+      public: true,
+      label: 'Toggle Shortcut',
+    },
+    autoHide: {
+      type: SettingItemType.Bool,
+      value: false,
+      description: 'Automatically hide the panel when there is no content',
+      section: 'outline.settings',
+      public: true,
+      label: 'Auto Hide',
+    },
+    headerDepth: {
+      type: SettingItemType.Int,
+      value: 6,
+      description: 'Header depth',
+      section: 'outline.settings',
+      public: true,
+      label: 'Header Depth',
+    },
+    fontFamily: {
+      type: SettingItemType.String,
+      value: 'var(--joplin-font-family)',
+      description: 'var(--joplin-font-family)',
+      section: 'outline.settings',
+      public: true,
+      label: 'Font Family',
+    },
+    fontSize: {
+      type: SettingItemType.Int,
+      value: '10',
+      description: 'Default: 10pt',
+      section: 'outline.settings',
+      public: true,
+      label: 'Font Size (pt)',
+    },
+    fontWeight: {
+      type: SettingItemType.String,
+      value: 'normal',
+      description: 'bold/normal/lighter',
+      section: 'outline.settings',
+      public: true,
+      label: 'Font Weight',
+    },
+    fontColor: {
+      type: SettingItemType.String,
+      value: 'var(--joplin-color)',
+      description: 'var(--joplin-color)',
+      section: 'outline.settings',
+      public: true,
+      label: 'Font Color',
+    },
+    bgColor: {
+      type: SettingItemType.String,
+      value: 'var(--joplin-background-color)',
+      description: 'var(--joplin-background-color)',
+      section: 'outline.settings',
+      public: true,
+      label: 'Background Color',
+    },
+    disableLinewrap: {
+      type: SettingItemType.Bool,
+      value: false,
+      description: 'Disable the linewrap',
+      section: 'outline.settings',
+      public: true,
+      label: 'Disable Linewrap',
+    },
+    showNumber: {
+      type: SettingItemType.Bool,
+      value: false,
+      description: 'show numbered headers',
+      section: 'outline.settings',
+      public: true,
+      label: 'Show Number',
+      advanced: true,
+    },
+    numberStyle: {
+      type: SettingItemType.String,
+      value: 'font-weight: normal; font-style: normal',
+      description: 'font-weight: normal; font-style: normal',
+      section: 'outline.settings',
+      public: true,
+      label: 'Number <i> Style',
+      advanced: true,
+    },
+    h1Prefix: {
+      type: SettingItemType.String,
+      value: '',
+      section: 'outline.settings',
+      public: true,
+      label: 'H1 Prefix',
+      advanced: true,
+    },
+    h2Prefix: {
+      type: SettingItemType.String,
+      value: '',
+      section: 'outline.settings',
+      public: true,
+      label: 'H2 Prefix',
+      advanced: true,
+    },
+    h3Prefix: {
+      type: SettingItemType.String,
+      value: '',
+      section: 'outline.settings',
+      public: true,
+      label: 'H3 Prefix',
+      advanced: true,
+    },
+    h4Prefix: {
+      type: SettingItemType.String,
+      value: '',
+      section: 'outline.settings',
+      public: true,
+      label: 'H4 Prefix',
+      advanced: true,
+    },
+    h5Prefix: {
+      type: SettingItemType.String,
+      value: '',
+      section: 'outline.settings',
+      public: true,
+      label: 'H5 Prefix',
+      advanced: true,
+    },
+    h6Prefix: {
+      type: SettingItemType.String,
+      value: '',
+      section: 'outline.settings',
+      public: true,
+      label: 'H6 Prefix',
+      advanced: true,
+    },
   });
 }
 
