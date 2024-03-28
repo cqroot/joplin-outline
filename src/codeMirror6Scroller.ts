ca74eb4f01c37be0b81aef187d6de1dc1e91618a
alondmnt <alondmnt@users.noreply.github.com>
2024-03-29 00:02:02 +1100

    new: CodeMirror6 support

---
+++ src/codeMirror6Scroller.ts
@@ -0,0 +1,34 @@
+import type { ContentScriptContext, MarkdownEditorContentScriptModule } from 'api/types';
+import { EditorView } from '@codemirror/view';
+
+// modified from: https://github.com/personalizedrefrigerator/bug-report/tree/example/plugin-scroll-to-line
+export default (context: ContentScriptContext): MarkdownEditorContentScriptModule => {
+	return {
+		plugin: (editorControl: any) => {
+			if (editorControl.cm6) {
+        // Running in CM6
+        editorControl.registerCommand('scrollToLine', (lineNumber: number) => {
+          const editor: EditorView = editorControl.editor;
+          console.log('scrollToLine', lineNumber);
+
+          // Bounds checking
+          if (lineNumber < 0) {
+            lineNumber = 0;
+          }
+          if (lineNumber > editor.state.doc.lines) {
+            lineNumber = editor.state.doc.lines;
+          }
+
+          // Scroll to line, place the line at the *top* of the editor
+          const lineInfo = editor.state.doc.line(lineNumber);
+          editor.dispatch(editor.state.update({
+              effects: EditorView.scrollIntoView(lineInfo.from, {y: 'start'})
+          }));
+        });
+
+      } else {
+        // Running in CM5, see codeMirror5Scroller.js
+      }
+		},
+	};
+};
