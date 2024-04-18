df7fff822b91107137453426f7929955ff798c35
alondmnt <alondmnt@users.noreply.github.com>
2024-04-18 17:48:21 +1000

    chore: formatting

--- src/codeMirror6Scroller.ts
+++ src/codeMirror6Scroller.ts
@@ -3,9 +3,9 @@ import { EditorView } from '@codemirror/view';
 
 // modified from: https://github.com/personalizedrefrigerator/bug-report/tree/example/plugin-scroll-to-line
 export default (context: ContentScriptContext): MarkdownEditorContentScriptModule => {
-	return {
-		plugin: (editorControl: any) => {
-			if (!editorControl.cm6) { return; }
+  return {
+    plugin: (editorControl: any) => {
+      if (!editorControl.cm6) { return; }
 
       // Running in CM6
       editorControl.registerCommand('scrollToLine', (lineNumber: number) => {
@@ -23,9 +23,9 @@ export default (context: ContentScriptContext): MarkdownEditorContentScriptModul
         // Scroll to line, place the line at the *top* of the editor
         const lineInfo = editor.state.doc.line(lineNumber);
         editor.dispatch(editor.state.update({
-            effects: EditorView.scrollIntoView(lineInfo.from, {y: 'start'})
+          effects: EditorView.scrollIntoView(lineInfo.from, {y: 'start'})
         }));
       });
-		},
-	};
+    },
+  };
 };
